341cebf0e7d9f494fd9654be2e77d4a403c19466
Plaligraphy <evanac21@gmail.com>
2020-09-29 12:15:52 -0700

    Automated Push!

--- savehandler.js
+++ savehandler.js
@@ -1,7 +1,20 @@
 var fs = require("fs");
+var dat = "";
 function save() {
+//write
+  
+
+  //read
     fs.readFile("save", "utf-8", (err, data) => {
         console.log(data);
       });
+}
+function write(dat) {
+  fs.writeFile('save', dat, function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+   // console.log(data);
+  });
 }
 module.exports = { save }
