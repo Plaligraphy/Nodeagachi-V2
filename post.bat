653b54ac3d790c56283db265400eaec8e58f018e
evanac21 <evanac21@gmail.com>
2023-04-15 15:03:37 -0700

    Fixed

--- post.bat
+++ post.bat
@@ -1,7 +1,7 @@
 @echo off
-echo "Posting file..."
+echo Posting file...
 git add *
-set /P id=Enter Commit Comments:
+set /P id=Enter Commit Comments: 
 git commit -m %id% 
 git push
-echo "Commit Completed! Exitting..."
+echo Commit Completed! Exitting...
